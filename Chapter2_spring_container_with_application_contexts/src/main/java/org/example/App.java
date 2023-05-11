c5149e80263d77ea2eef550c1b4fd32987bcf63e
Rasmi <rala8730@colorado.edu>
2023-05-10 18:35:49 -0600

    Add files via upload

---
+++ Chapter2_spring_container_with_application_contexts/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext ctx= new FileSystemXmlApplicationContext("application-context.xml");
+
+    }
+}
